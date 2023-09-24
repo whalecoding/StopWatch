da67ea0b55390bb100d23d5fb3c88350d1c95bcb
whalecoding <89170523+whalecoding@users.noreply.github.com>
2023-09-24 14:08:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ function formatTime(ms) {
 }
 
 function formatLapTime(ms) {
-    console.log(ms)
     const seconds = Math.floor((ms % 60000) / 1000);
     const milliseconds = ms % 1000;
     return `${String(seconds).padStart(2, '0')}.${String(milliseconds).padStart(3, '0')}`;
